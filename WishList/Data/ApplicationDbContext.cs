9873d4c4a81db394fce8a647e8dc70f1b962de24
Calvin Mills <calvin.mills@axa-ppp.co.uk>
2018-07-17 10:58:16 +0100

    4.4 and 4.5

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext: DbContext
     {
+        public List<Item> Items;
         public ApplicationDbContext(DbContextOptions options): base(options)    
         {
-
+            
         }
     }
 }
